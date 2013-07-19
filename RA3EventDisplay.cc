5d44b6406caad8d00ac11f678571369451d0951b
Yutaro <yutaro.iiyama@cern.ch>
2013-07-19 17:59:48 +0200

    Reshuffling a bit; added multiple MET

--- RA3EventDisplay.cc
+++ RA3EventDisplay.cc
@@ -23,6 +23,7 @@
 
 double const sizeNorm(2.);
 double const etaMax(4.);
+TString const HtDescription("H_{T}: P_{T}^{j} > 30 GeV && |#eta^{j}| < 3");
 
 class RA3EventDisplay {
 public:
@@ -30,26 +31,32 @@ public:
   ~RA3EventDisplay();
 
   int addPath(TString const&);
-  void setPtThreshold(double _pt) { ptThreshold_ = _pt; }
+  void setPtThreshold(double);
   void setPFJetCollection(TString const&);
+  void addMet(TString const&, bool = true, int = 0);
+  bool setMainMet(TString const&);
 
   bool showEvent(unsigned, unsigned);
-  bool refresh();
   bool showNextEvent();
+  bool refresh();
 
   double mass(unsigned, unsigned = -1, unsigned = -1, unsigned = -1) const;
   double mt(unsigned, unsigned = -1, unsigned = -1, unsigned = -1) const;
+  TString const& getMainMet() const { return mainMet_; }
 
   void print(TString const& _fileName) { canvas_.Print(_fileName); }
 
 private:
   void showEvent_();
+  void formLegend_(bool = false);
 
   TChain mainTree_;
   TChain scanTree_;
 
   double ptThreshold_;
-  TString pfJetCollection_;
+  TString pfJetCollectionTag_;
+  std::map<TString, int> metTags_;
+  TString mainMet_;
 
   unsigned runNumber_;
   unsigned luminosityBlockNumber_;
@@ -60,10 +67,11 @@ private:
 
   TCanvas canvas_;
   TH2F etaPhiField_;
-  TLine metLine_;
+  TPaveText eventInfo_;
+  TPaveText notes_;
+
   TLegend sizeLegend_;
   TLegend colorLegend_;
-  TPaveText eventInfo_;
 
   TObjArray garbageCollection_;
 };
@@ -71,25 +79,27 @@ private:
 RA3EventDisplay::RA3EventDisplay() :
   mainTree_("susyTree"),
   scanTree_("susyTree"),
-  ptThreshold_(0.),
-  pfJetCollection_("ak5"),
+  ptThreshold_(1.),
+  pfJetCollectionTag_("ak5"),
+  metTags_(),
+  mainMet_("pfType01CorrectedMet"),
   runNumber_(0),
   luminosityBlockNumber_(0),
   eventNumber_(0),
   currentEntry_(-1),
   event_(),
-  canvas_("evdisp", "RA3 Event Display"),
+  canvas_("evdisp", "RA3 Event Display", 800, 600),
   etaPhiField_("evdisp", ";#eta;#phi", 100, -etaMax, etaMax, 100, -TMath::Pi(), TMath::Pi()),
-  metLine_(-etaMax, 0., etaMax, 0.),
-  sizeLegend_(0.82, 0.35, 0.98, 0.5),
-  colorLegend_(0.82, 0.05, 0.98, 0.3),
   eventInfo_(0.82, 0.8, 0.98, 0.95, "brNDC"),
+  notes_(0.82, 0.7, 0.98, 0.75, "brNDC"),
+  sizeLegend_(0.82, 0.4, 0.98, 0.55),
+  colorLegend_(0.82, 0.05, 0.98, 0.35),
   garbageCollection_()
 {
   mainTree_.SetBranchStatus("*", 0);
   mainTree_.SetBranchStatus("pfParticles*", 1);
   mainTree_.SetBranchStatus("pfJets*", 1);
-  mainTree_.SetBranchStatus("met_pfType01CorrectedMet*", 1);
+  mainTree_.SetBranchStatus("met*", 1);
 
   scanTree_.SetBranchStatus("*", 0);
   scanTree_.SetBranchStatus("runNumber", 1);
@@ -98,6 +108,8 @@ RA3EventDisplay::RA3EventDisplay() :
   scanTree_.SetBranchAddress("runNumber", &runNumber_);
   scanTree_.SetBranchAddress("luminosityBlockNumber", &luminosityBlockNumber_);
   scanTree_.SetBranchAddress("eventNumber", &eventNumber_);
+
+  metTags_[mainMet_] = kRed;
 
   canvas_.SetGrid(1, 1);
   canvas_.SetMargin(0.05, 0.2, 0.05, 0.05);
@@ -111,63 +123,23 @@ RA3EventDisplay::RA3EventDisplay() :
   etaPhiField_.GetXaxis()->SetTitleSize(0.05);
   etaPhiField_.GetYaxis()->SetTitleSize(0.05);
 
-  metLine_.SetLineWidth(2);
-  metLine_.SetLineColor(kRed);
-  metLine_.SetLineStyle(kDashed);
-
-  TMarker* tenGeV(new TMarker(0., 0., kFullCircle));
-  TMarker* hundredGeV(new TMarker(0., 0., kFullCircle));
-  tenGeV->SetMarkerSize(sizeNorm);
-  hundredGeV->SetMarkerSize(sizeNorm * 2.);
-
-  sizeLegend_.SetBorderSize(0);
-  sizeLegend_.SetFillStyle(0);
-  sizeLegend_.SetTextAlign(32); // right, center
-  sizeLegend_.AddEntry(tenGeV, "10 GeV", "P");
-  sizeLegend_.AddEntry(hundredGeV, "100 GeV", "P");
-
-  TMarker* photon(new TMarker(0., 0., kFullCircle));
-  TMarker* electron(new TMarker(0., 0., kFullCircle));
-  TMarker* muon(new TMarker(0., 0., kFullCircle));
-  TMarker* jetHadron(new TMarker(0., 0., kFullCircle));
-  TMarker* strayHadron(new TMarker(0., 0., kFullCircle));
-  TMarker* jet(new TMarker(0., 0., kOpenCircle));
-  photon->SetMarkerColor(kGreen);
-  electron->SetMarkerColor(kBlue);
-  muon->SetMarkerColor(kRed);
-  jetHadron->SetMarkerColor(kOrange);
-  strayHadron->SetMarkerColor(kBlack);
-  jet->SetMarkerColor(kOrange);
-
-  colorLegend_.AddEntry(photon, "Photon", "P");
-  colorLegend_.AddEntry(electron, "Electron", "P");
-  colorLegend_.AddEntry(muon, "Muon", "P");
-  colorLegend_.AddEntry(jetHadron, "Hadron in jet", "P");
-  colorLegend_.AddEntry(strayHadron, "Hadron not in jet", "P");
-  colorLegend_.AddEntry(jet, pfJetCollection_ + "PFJet", "P");
-  colorLegend_.AddEntry(&metLine_, "MET", "L");
-
   eventInfo_.SetBorderSize(1);
 
+  notes_.SetBorderSize(1);
+  notes_.SetTextAlign(12); // left, center
+  notes_.SetFillStyle(0);
+  notes_.AddText(HtDescription);
+
   garbageCollection_.SetOwner(true);
+
+  formLegend_();
 }
 
 RA3EventDisplay::~RA3EventDisplay()
 {
   event_.releaseTree(mainTree_);
 
-  TList* colorList(colorLegend_.GetListOfPrimitives());
-  for(int iL(0); iL != colorList->GetEntries(); ++iL){
-    TLegendEntry* entry(static_cast<TLegendEntry*>(colorList->At(iL)));
-    if(TString(entry->GetLabel()) == "MET") continue;
-    delete entry->GetObject();
-  }
-
-  TList* sizeList(sizeLegend_.GetListOfPrimitives());
-  for(int iL(0); iL != sizeList->GetEntries(); ++iL){
-    TLegendEntry* entry(static_cast<TLegendEntry*>(sizeList->At(iL)));
-    delete entry->GetObject();
-  }
+  formLegend_(true);
 }
 
 int
@@ -184,19 +156,83 @@ RA3EventDisplay::addPath(TString const& _path)
 }
 
 void
-RA3EventDisplay::setPFJetCollection(TString const& _pfJetCollection)
+RA3EventDisplay::setPtThreshold(double _ptThresh)
 {
-  TList* colorList(colorLegend_.GetListOfPrimitives());
-  for(int iL(0); iL != colorList->GetEntries(); ++iL){
-    TLegendEntry* entry(static_cast<TLegendEntry*>(colorList->At(iL)));
-    TString label(entry->GetLabel());
-    if(label.Contains("PFJet")){
-      label.ReplaceAll(pfJetCollection_, _pfJetCollection);
-      entry->SetLabel(label);
+  if(_ptThresh < 1.){
+    std::cerr << "Cannot set threshold to lower than 1 GeV" << std::endl;
+    return;
+  }
+  ptThreshold_ = _ptThresh;
+}
+
+void
+RA3EventDisplay::setPFJetCollection(TString const& _pfJetCollectionTag)
+{
+  pfJetCollectionTag_ = _pfJetCollectionTag;
+
+  formLegend_();
+
+  refresh();
+}
+
+void
+RA3EventDisplay::addMet(TString const& _metName, bool _add/* = true*/, int _color/* = 0*/)
+{
+  if(_add){
+    int color(_color);
+    if(color == 0){
+      color = kRed;
+      while(true){
+        std::map<TString, int>::iterator itr(metTags_.begin());
+        for(; itr != metTags_.end(); ++itr)
+          if(itr->second == color) break;
+
+        if(itr != metTags_.end()) ++color;
+        else break;
+      }
+    }
+
+    metTags_[_metName] = color;
+
+    colorLegend_.SetY2(colorLegend_.GetY2() + 0.05);
+  }
+  else{
+    std::map<TString, int>::iterator itr(metTags_.find(_metName));
+    if(itr == metTags_.end()) return;
+    metTags_.erase(itr);
+    colorLegend_.SetY2(colorLegend_.GetY2() - 0.05);
+
+    if(mainMet_ == _metName){
+      if(metTags_.size() != 0)
+        mainMet_ = metTags_.begin()->first;
+      else
+        mainMet_ = "";
     }
   }
 
-  pfJetCollection_ = _pfJetCollection;
+  formLegend_();
+
+  setMainMet(mainMet_);
+
+  refresh();
+}
+
+bool
+RA3EventDisplay::setMainMet(TString const& _metTag)
+{
+  if(metTags_.find(_metTag) == metTags_.end()) return false;
+
+  mainMet_ = _metTag;
+
+  notes_.Clear();
+  notes_.AddText(HtDescription);
+  if(metTags_.size() != 1) notes_.AddText("MET: " + mainMet_);
+
+  formLegend_();
+
+  refresh();
+
+  return true;
 }
 
 bool
@@ -255,6 +291,45 @@ RA3EventDisplay::refresh()
     return false;
 }
 
+double
+RA3EventDisplay::mass(unsigned _p0, unsigned _p1/* = -1*/, unsigned _p2/* = -1*/, unsigned _p3/* = -1*/) const
+{
+  if(currentEntry_ == -1) return 0.;
+
+  susy::PFParticleCollection const& particles(event_.pfParticles);
+
+  TLorentzVector sumP;
+  if(_p0 < particles.size()) sumP += particles[_p0].momentum;
+  if(_p1 < particles.size()) sumP += particles[_p1].momentum;
+  if(_p2 < particles.size()) sumP += particles[_p2].momentum;
+  if(_p3 < particles.size()) sumP += particles[_p3].momentum;
+
+  return sumP.M();
+}
+
+double
+RA3EventDisplay::mt(unsigned _p0, unsigned _p1/* = -1*/, unsigned _p2/* = -1*/, unsigned _p3/* = -1*/) const
+{
+  if(currentEntry_ == -1) return 0.;
+
+  susy::PFParticleCollection const& particles(event_.pfParticles);
+  susy::METMap::const_iterator mItr(event_.metMap.find(mainMet_));
+  if(mItr == event_.metMap.end()){
+    std::cerr << mainMet_ << " not found" << std::endl;
+    return 0.;
+  }
+
+  TVector2 const& met(mItr->second.mEt);
+
+  TVector2 sumPt;
+  if(_p0 < particles.size()) sumPt += TVector2(particles[_p0].momentum.X(), particles[_p0].momentum.Y());
+  if(_p1 < particles.size()) sumPt += TVector2(particles[_p1].momentum.X(), particles[_p1].momentum.Y());
+  if(_p2 < particles.size()) sumPt += TVector2(particles[_p2].momentum.X(), particles[_p2].momentum.Y());
+  if(_p3 < particles.size()) sumPt += TVector2(particles[_p3].momentum.X(), particles[_p3].momentum.Y());
+
+  return TMath::Sqrt(TMath::Power(sumPt.Mod() + met.Mod(), 2.) - (sumPt + met).Mod2());
+}
+
 void
 RA3EventDisplay::showEvent_()
 {
@@ -264,14 +339,21 @@ RA3EventDisplay::showEvent_()
   etaPhiField_.SetTitle(TString::Format("Run %d, Lumi %d, Event %d", runNumber_, luminosityBlockNumber_, eventNumber_));
   etaPhiField_.Draw();
 
-  sizeLegend_.Draw();
   colorLegend_.Draw();
+  sizeLegend_.Draw();
+  notes_.Draw();
 
   event_.getEntry(currentEntry_);
 
   susy::PFParticleCollection const& particles(event_.pfParticles);
-  susy::PFJetCollection const& pfJets(event_.pfJets[pfJetCollection_]);
-  susy::MET const& met(event_.metMap["pfType01CorrectedMet"]);
+  susy::PFJetCollectionMap::const_iterator jetColItr(event_.pfJets.find(pfJetCollectionTag_));
+  if(jetColItr == event_.pfJets.end()){
+    std::cerr << pfJetCollectionTag_ << " not found" << std::endl;
+    canvas_.Clear();
+    return;
+  }
+
+  susy::PFJetCollection const& pfJets(jetColItr->second);
 
   unsigned nPF(particles.size());
   unsigned nJet(pfJets.size());
@@ -283,6 +365,8 @@ RA3EventDisplay::showEvent_()
 
     TLorentzVector const& p(jet.momentum);
     TLorentzVector corrP(jet.momentum * jet.jecScaleFactors.find("L1FastL2L3")->second);
+    if(corrP.Pt() < 1.) continue; // never happens, but just for mathematical sanity..
+
     if(corrP.Pt() > 30.
        && TMath::Abs(corrP.Eta()) < 3.
        && jet.chargedHadronEnergy / p.E() > 0.
@@ -298,7 +382,9 @@ RA3EventDisplay::showEvent_()
     for(unsigned iC(0); iC != nC; ++iC)
       jetConstituents.insert(list[iC]);
 
-    TEllipse* circle(new TEllipse(jet.momentum.Eta(), jet.momentum.Phi(), TMath::Sqrt(jet.jetArea / TMath::Pi())));
+    // Marker size 1 is absolute 8 pix. Ellipse radius is given in dR, so the conversion factor is:
+    // radius = 8 * log10(jetEt) * sizeNorm * 2etaMax / (windowWidth * 0.75) 
+    TEllipse* circle(new TEllipse(jet.momentum.Eta(), jet.momentum.Phi(), 8. * TMath::Log10(corrP.Pt()) * sizeNorm * 2. * etaMax / canvas_.GetWindowWidth() / 0.75));
     circle->SetLineColor(kOrange);
     circle->SetLineWidth(2);
     circle->SetLineStyle(kSolid);
@@ -362,50 +448,95 @@ RA3EventDisplay::showEvent_()
     garbageCollection_.Add(marker);
   }
 
-  metLine_.SetY1(TVector2::Phi_mpi_pi(met.mEt.Phi()));
-  metLine_.SetY2(TVector2::Phi_mpi_pi(met.mEt.Phi()));
+  double metVal(0.);
 
-  metLine_.Draw();
+  for(std::map<TString, int>::iterator tItr(metTags_.begin()); tItr != metTags_.end(); ++tItr){
+    susy::METMap::const_iterator metItr(event_.metMap.find(tItr->first));
+    if(metItr == event_.metMap.end()){
+      std::cerr << tItr->first << " not found" << std::endl;
+      canvas_.Clear();
+      return;
+    }
+
+    susy::MET const& met(metItr->second);
+
+    TLine* metLine(new TLine(-etaMax, TVector2::Phi_mpi_pi(met.mEt.Phi()), etaMax, TVector2::Phi_mpi_pi(met.mEt.Phi())));
+    metLine->SetLineWidth(2);
+    metLine->SetLineColor(tItr->second);
+    metLine->SetLineStyle(kDashed);
+
+    metLine->Draw();
+
+    garbageCollection_.Add(metLine);
+
+    if(tItr->first == mainMet_) metVal = met.met();
+  }
 
   eventInfo_.Clear();
 
-  eventInfo_.AddText(TString::Format("MET = %.2f GeV", met.met()));
+  eventInfo_.AddText(TString::Format("MET = %.2f GeV", metVal));
   eventInfo_.AddText(TString::Format("HT = %.2f GeV", ht));
 
   eventInfo_.Draw();
 }
 
-double
-RA3EventDisplay::mass(unsigned _p0, unsigned _p1/* = -1*/, unsigned _p2/* = -1*/, unsigned _p3/* = -1*/) const
+void
+RA3EventDisplay::formLegend_(bool _clearOnly/* = false*/)
 {
-  if(currentEntry_ == -1) return 0.;
+  TList* colorList(colorLegend_.GetListOfPrimitives());
+  for(int iL(0); iL != colorList->GetEntries(); ++iL){
+    TLegendEntry* entry(static_cast<TLegendEntry*>(colorList->At(iL)));
+    delete entry->GetObject();
+  }
 
-  susy::PFParticleCollection const& particles(event_.pfParticles);
+  TList* sizeList(sizeLegend_.GetListOfPrimitives());
+  for(int iL(0); iL != sizeList->GetEntries(); ++iL){
+    TLegendEntry* entry(static_cast<TLegendEntry*>(sizeList->At(iL)));
+    delete entry->GetObject();
+  }
 
-  TLorentzVector sumP;
-  if(_p0 < particles.size()) sumP += particles[_p0].momentum;
-  if(_p1 < particles.size()) sumP += particles[_p1].momentum;
-  if(_p2 < particles.size()) sumP += particles[_p2].momentum;
-  if(_p3 < particles.size()) sumP += particles[_p3].momentum;
+  colorLegend_.Clear();
+  sizeLegend_.Clear();
 
-  return sumP.M();
-}
+  if(_clearOnly) return;
 
-double
-RA3EventDisplay::mt(unsigned _p0, unsigned _p1/* = -1*/, unsigned _p2/* = -1*/, unsigned _p3/* = -1*/) const
-{
-  if(currentEntry_ == -1) return 0.;
+  TMarker* tenGeV(new TMarker(0., 0., kFullCircle));
+  TMarker* hundredGeV(new TMarker(0., 0., kFullCircle));
+  tenGeV->SetMarkerSize(sizeNorm);
+  hundredGeV->SetMarkerSize(sizeNorm * 2.);
 
-  susy::PFParticleCollection const& particles(event_.pfParticles);
-  susy::METMap::const_iterator mItr(event_.metMap.find("pfType01CorrectedMet"));
-  if(mItr == event_.metMap.end()) return 0.;
-  TVector2 const& met(mItr->second.mEt);
+  sizeLegend_.SetBorderSize(0);
+  sizeLegend_.SetFillStyle(0);
+  sizeLegend_.SetTextAlign(32); // right, center
+  sizeLegend_.AddEntry(tenGeV, "10 GeV", "P");
+  sizeLegend_.AddEntry(hundredGeV, "100 GeV", "P");
 
-  TVector2 sumPt;
-  if(_p0 < particles.size()) sumPt += TVector2(particles[_p0].momentum.X(), particles[_p0].momentum.Y());
-  if(_p1 < particles.size()) sumPt += TVector2(particles[_p1].momentum.X(), particles[_p1].momentum.Y());
-  if(_p2 < particles.size()) sumPt += TVector2(particles[_p2].momentum.X(), particles[_p2].momentum.Y());
-  if(_p3 < particles.size()) sumPt += TVector2(particles[_p3].momentum.X(), particles[_p3].momentum.Y());
+  TMarker* photon(new TMarker(0., 0., kFullCircle));
+  TMarker* electron(new TMarker(0., 0., kFullCircle));
+  TMarker* muon(new TMarker(0., 0., kFullCircle));
+  TMarker* jetHadron(new TMarker(0., 0., kFullCircle));
+  TMarker* strayHadron(new TMarker(0., 0., kFullCircle));
+  TMarker* jet(new TMarker(0., 0., kOpenCircle));
+  photon->SetMarkerColor(kGreen);
+  electron->SetMarkerColor(kBlue);
+  muon->SetMarkerColor(kRed);
+  jetHadron->SetMarkerColor(kOrange);
+  strayHadron->SetMarkerColor(kBlack);
+  jet->SetMarkerColor(kOrange);
 
-  return TMath::Sqrt(TMath::Power(sumPt.Mod() + met.Mod(), 2.) - (sumPt + met).Mod2());
+  colorLegend_.AddEntry(photon, "Photon", "P");
+  colorLegend_.AddEntry(electron, "Electron", "P");
+  colorLegend_.AddEntry(muon, "Muon", "P");
+  colorLegend_.AddEntry(jetHadron, "Hadron in jet", "P");
+  colorLegend_.AddEntry(strayHadron, "Hadron not in jet", "P");
+  colorLegend_.AddEntry(jet, pfJetCollectionTag_ + "PFJet", "P");
+
+  for(std::map<TString, int>::iterator mItr(metTags_.begin()); mItr != metTags_.end(); ++mItr){
+    TLine* metLine(new TLine(-etaMax, 0., etaMax, 0.));
+    metLine->SetLineWidth(2);
+    metLine->SetLineColor(mItr->second);
+    metLine->SetLineStyle(kDashed);
+
+    colorLegend_.AddEntry(metLine, mItr->first, "L");
+  }
 }
